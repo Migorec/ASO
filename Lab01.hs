760ff37dc01a977a85069799938c103169a23653
MigOrec <MigOrec@gmail.com>
2012-10-26 11:35:43 +0400

    засчитанная первая лаба

--- Lab01.hs
+++ Lab01.hs
@@ -4,7 +4,7 @@ module Main (choose, awfulMult, main,lab01)where
 import Math.Combinatorics.Exact.Binomial
 import Numeric.Tools.Integration
 import Data.Maybe (fromJust)
-import Data.List (zipWith4, zipWith7, intersperse)
+import Data.List (zipWith5, zipWith7, intersperse)
 import Text.Printf
 import System.Cmd
 import System.Environment
@@ -36,7 +36,7 @@ lab01 :: Double -> Integer -> IO ()
 lab01 p n = do 
                writeFile "test.dat" $ unlines ("#K\tXi\tPB\tPG":toFile)
                writeFile "result.tex" $ unlines $ intersperse "\\hline" toTex
-               _ <- system  $ "(echo 'set x2tics' && echo 'set x2range [  " ++ show (-fn*p/sqrt(fn*p*q))++":" ++ show ((fn-fn*p)/sqrt(fn*p*q))++ "]' && echo 'set label \"p=" ++ show p ++ "\\\\nq="++show q++"\\\\nn="++show n ++"\" at screen 0.2,0.7' && cat plot.gp) | gnuplot && ps2pdf -dEPSCrop plot.eps plot.pdf && pdflatex report.tex"       
+               _ <- system  $ "(echo 'set x2tics' && echo 'set xrange [0:" ++ (show n) ++"]' && echo 'set x2range [  " ++ show (-fn*p/sqrt(fn*p*q))++":" ++ show ((fn-fn*p)/sqrt(fn*p*q))++ "]' && echo 'set label \"p=" ++ show p ++ "\\\\nq="++show q++"\\\\nn="++show n ++"\" at screen 0.2,0.7' && cat plot.gp) | gnuplot && ps2pdf -dEPSCrop plot.eps plot.pdf && pdflatex report.tex"       
                return ()
     where q = 1 - p
           rp = approxRational p 0.0000001
@@ -54,10 +54,11 @@ lab01 p n = do
           dPs = zipWith (\pB pG -> abs $ pB - pG) pBs pGs
           fBs = map (sqrt(fn*p*q)*) pBs
           fGs = map (\x -> exp(-x*x/2)/sqrt(2*pi)) xis
-          toFile = zipWith4 (\k xi pB pG -> show k ++ "\t" ++ 
-                                            show xi ++ "\t" ++
-                                            show pB ++ "\t" ++
-                                            show pG) ks xis pBs pGs
+          toFile = zipWith5 (\k xi pB fB fG -> show k ++ "\t" ++ 
+                                               show xi ++ "\t" ++
+                                               show pB ++ "\t" ++
+                                               show fB ++ "\t" ++
+                                               show fG) ks xis pBs fBs fGs
           toTex = zipWith7 (\k xi pB pG dP fB fG -> show k ++ " & " ++
                                                     printf "%.2f" xi ++ " & " ++
                                                     printf "%.4e" pB ++ " & " ++
--- plot.gp
+++ plot.gp
@@ -7,7 +7,7 @@ set x2label '$\xi$'
 set y2zeroaxis lt - 1
 set style line 22 lt 3 lw 4
 set style line 10 lt 1 lw 4
-plot "test.dat" using 1:3 title "$P_B$" ls 10, "test.dat" using 2:4 title "$P_G$" ls 22 with lines axes x2y1
+plot "test.dat" using 1:3 title "$P_B$" ls 10,"test.dat" using 1:4 with boxes title "$f_B$" ls 10, "test.dat" using 2:5 title "$f_G$" smooth cspline ls 22 with lines axes x2y1
 
 
 
